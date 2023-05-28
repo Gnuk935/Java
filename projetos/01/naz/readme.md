4a62ca9da2a013d124c8eb438287a107d64c9e5d
Victor Barros <89825674+Gnuk935@users.noreply.github.com>
2023-05-28 15:34:54 -0400

    view01

---
+++ projetos/01/naz/readme.md
@@ -0,0 +1,2 @@
+# Projeto Zanolli
+Esse projeto consiste em converter um codigo em JS (app script) para Java
---
+++ projetos/01/naz/src/com/barros/model/Dados.java
@@ -0,0 +1,95 @@
+package com.barros.model;
+
+public class Dados {
+	private String atleta;
+	private String estilo;
+	private String rotina;
+	private String tipoRotina;
+	private String naipe;
+	private int diaNascimeto;
+	private int mesNascimento;
+	private int anoNascimento;
+	private int idade;
+	private String faixaEtaria;
+	private String nivel;
+	private float peso;
+	private String academia;
+	public String getAtleta() {
+		return atleta;
+	}
+	public void setAtleta(String atleta) {
+		this.atleta = atleta;
+	}
+	public String getEstilo() {
+		return estilo;
+	}
+	public void setEstilo(String estilo) {
+		this.estilo = estilo;
+	}
+	public String getRotina() {
+		return rotina;
+	}
+	public void setRotina(String rotina) {
+		this.rotina = rotina;
+	}
+	public String getTipoRotina() {
+		return tipoRotina;
+	}
+	public void setTipoRotina(String tipoRotina) {
+		this.tipoRotina = tipoRotina;
+	}
+	public String getNaipe() {
+		return naipe;
+	}
+	public void setNaipe(String naipe) {
+		this.naipe = naipe;
+	}
+	public int getDiaNascimeto() {
+		return diaNascimeto;
+	}
+	public void setDiaNascimeto(int diaNascimeto) {
+		this.diaNascimeto = diaNascimeto;
+	}
+	public int getMesNascimento() {
+		return mesNascimento;
+	}
+	public void setMesNascimento(int mesNascimento) {
+		this.mesNascimento = mesNascimento;
+	}
+	public int getAnoNascimento() {
+		return anoNascimento;
+	}
+	public void setAnoNascimento(int anoNascimento) {
+		this.anoNascimento = anoNascimento;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getFaixaEtaria() {
+		return faixaEtaria;
+	}
+	public void setFaixaEtaria(String faixaEtaria) {
+		this.faixaEtaria = faixaEtaria;
+	}
+	public String getNivel() {
+		return nivel;
+	}
+	public void setNivel(String nivel) {
+		this.nivel = nivel;
+	}
+	public float getPeso() {
+		return peso;
+	}
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
+	public String getAcademia() {
+		return academia;
+	}
+	public void setAcademia(String academia) {
+		this.academia = academia;
+	}
+}
